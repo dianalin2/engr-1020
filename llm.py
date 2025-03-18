cd81a6133782f86dab39ad229c3166dfcd30fb18
Diana Lin <lindiana1206@gmail.com>
2025-03-18 13:47:20 -0400

    add generation of component attributes

--- llm.py
+++ llm.py
@@ -2,12 +2,18 @@
 from transformers import pipeline
 
 prompt = [
-    {"role": "system", "content": "You generate SysML v2 textual diagrams for JSON data that the user gives you. Respond with only the SysML v2 Textual Representation, not formatted in any way."}
+    {"role": "system", "content": "You generate SysML v2 textual diagrams for JSON data of hardware components that the user gives you. Respond with only the SysML v2 Textual Representation, not formatted in any way."}
 ]
 
-pipe = pipeline("text-generation", model="meta-llama/Llama-3.3-70B-Instruct")
+def generate_component_attributes(component):
+    pipe = pipeline("text-generation", model="meta-llama/Llama-3.3-70B-Instruct")
+    return pipe(prompt + [
+        { "role": "system", "content": "Please generate the attributes of the following component:" },
+        { "role": "user", "content": component }
+    ])[0]['generated_text']
 
 def generate_sysml(json_data):
+    pipe = pipeline("text-generation", model="meta-llama/Llama-3.3-70B-Instruct")
     return pipe(prompt + [{
         "role": "user",
         "content": json_data
