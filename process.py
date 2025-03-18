9140cf5ebc64370e627565905c9bece5b5af5f19
Jessica Su <aes6qb@virginia.edu>
2025-03-18 14:20:39 -0400

    add gui

---
+++ process.py
@@ -0,0 +1,15 @@
+import glob
+import pandas as pd
+
+def read_data():
+    df = pd.DataFrame()
+
+    for filename in glob.glob('data/*.csv'):
+        df = pd.concat([df, pd.read_csv(filename)])
+
+    return df
+
+
+# print json representation of data
+df = read_data()
+print(df.to_json(orient='records'))
