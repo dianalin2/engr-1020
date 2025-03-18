4c18fff07d4544e95fcdd7480664cb1ca8bee273
Diana Lin <lindiana1206@gmail.com>
2025-03-18 15:07:38 -0400

    add comments

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 import glob
 import pandas as pd
 
+# read all csv files in data folder into pandas dataframe
 def read_data():
     df = pd.DataFrame()
 
@@ -8,7 +9,6 @@ def read_data():
         df = pd.concat([df, pd.read_csv(filename)])
 
     return df
-
 
 # print json representation of data
 df = read_data()
