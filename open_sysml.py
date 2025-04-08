c63b3089fcfa8bce24576518f13e564fa8e1725f
Jessica Su <aes6qb@virginia.edu>
2025-04-08 13:09:18 -0400

    small changes

--- open_sysml.py
+++ open_sysml.py
@@ -3,8 +3,6 @@ from pymongo.server_api import ServerApi
 import pymongo
 from dotenv import load_dotenv
 import os
-from llm import generate_component_attributes, generate_sysml
-import json
 
 load_dotenv()
 
@@ -18,14 +16,9 @@ db = client["engr1020"]
 class_spec = db["component_class_spec"]
 
 data = db["data"]
-# generate sysml v2 textual class specification for a hardware component based on the names of the fields in db["data"]
-if class_spec.count_documents({}) == 0:
-    uml_class = generate_component_attributes(data.find_one().keys())
-    class_spec.insert_one({"class_spec": uml_class})
-else:
-    uml_class = class_spec.find_one()["class_spec"]
 
-# have some identifier that is unique so that data will only appear once in the database. then loop through the data and update with the new class spec
+uml_class = class_spec.find_one()["class_spec"]
+
 models = []
 db.data.create_index([("Asset Identifier", pymongo.ASCENDING), ("Serial Number"), ("Location ID")], unique=True)
 for doc in data.find():
@@ -41,11 +34,11 @@ import tempfile
 temp_dir = tempfile.mkdtemp()
 # print(f"Temporary directory created at: {temp_dir}")
 # create a file in the temp folder
-with open(os.path.join(temp_dir, "models.txt"), "w") as f:
+with open(os.path.join(temp_dir, "models.sysml"), "w") as f:
     # print(f"Writing models to {os.path.join(temp_dir, 'models.txt')}")
     f.write(uml_class + "\n")
     for model in models:
         f.write(model + "\n")
 
 # print the path to the file
-print(os.path.join(temp_dir, 'models.txt'))
+print(os.path.join(temp_dir, 'models.sysml'))
